ea9a39dfc2f18f9d5e94701da0daf94cdf0dc6e2
wangtongmeng <tongmengwang@163.com>
2022-11-17 05:47:06 +0800

    feat: 首页布局

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import App from "./App.vue"
 import router from "./router/index"
 import ElementPlus from "element-plus" // 这里我们直接全量引入
 import "element-plus/dist/index.css"
+// 初始化css，重置css默认样式
+import "normalize.css/normalize.css"
+// 全局css
+import "@/styles/index.scss"
+
 const app = createApp(App)
 
 app.use(ElementPlus).use(router).use(createPinia()).mount("#app")
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router"
+import Layout from "@/layout/index.vue"
 export default createRouter({
   history: createWebHistory(),
   routes: [
+    {
+      path: "/",
+      component: Layout,
+      redirect: "/dashboard",
+      children: [
+        {
+          path: "dashboard",
+          name: "Dashboard",
+          component: () =>
+            import(
+              /* webpackChunkName: "dashboard" */
+              "@/views/dashboard/index.vue"
+            ),
+          meta: {
+            title: "Dashboard" // 需要配置声明文件否则无提示
+          }
+        }
+      ]
+    },
     {
       path: "/home",
       component: () => import("../views/Home.vue")

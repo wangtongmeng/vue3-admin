c6401c3b00b15fcef114440e07e6be72d9b4c57f
wangtongmeng <tongmengwang@163.com>
2022-11-16 08:38:09 +0800

    feat: 项目集成

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "@vue/prettier",
+    "./.eslintrc-auto-import.json",
+  ],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false, // 使用单引号
+        semi: false, // 末尾添加分号
+        tabWidth: 2,
+        trailingComma: "none",
+        useTabs: false,
+        endOfLine: "auto",
+      },
+    ],
+  },
+};

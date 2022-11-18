753d4945ebfa95cdd91f81ba2abf4437fa0cdbe7
wangtongmeng <tongmengwang@163.com>
2022-11-18 17:29:44 +0800

    feat: sidebar 组件基本样式及配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,108 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 import Layout from "@/layout/index.vue"
+
+// 看做是异步路由
+export const asyncRoutes: Array<RouteRecordRaw> = [
+  {
+    path: "/documentation",
+    component: Layout, // 布局组件作为一级路由
+    redirect: "/documentation/index",
+    children: [
+      {
+        path: "index",
+        name: "Documentation",
+        component: () =>
+          import(
+            /* webpackChunkName: "documentation" */ "@/views/documentation/index.vue"
+          ),
+        meta: {
+          title: "Documentation",
+          icon: "documentation"
+        }
+      }
+    ]
+  },
+  {
+    path: "/guide",
+    component: Layout,
+    redirect: "/guide/index",
+    children: [
+      {
+        path: "index",
+        name: "Guide",
+        component: () =>
+          import(/* webpackChunkName: "guide" */ "@/views/guide/index.vue"),
+        meta: {
+          title: "Guide",
+          icon: "guide"
+        }
+      }
+    ]
+  },
+  {
+    path: "/system",
+    component: Layout,
+    redirect: "/system/user",
+    meta: {
+      title: "System",
+      icon: "lock"
+    },
+    children: [
+      {
+        path: "menu",
+        component: () =>
+          import(/* webpackChunkName: "menu" */ "@/views/system/menu.vue"),
+        meta: {
+          title: "Menu Management",
+          icon: "list"
+        }
+      },
+      {
+        path: "role",
+        component: () =>
+          import(/* webpackChunkName: "role" */ "@/views/system/role.vue"),
+        meta: {
+          title: "Role Management",
+          icon: "list"
+        }
+      },
+      {
+        path: "user",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "@/views/system/user.vue"),
+        meta: {
+          title: "User Management",
+          icon: "list"
+        }
+      }
+    ]
+  }
+]
+
+const constantRoutes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () =>
+          import(
+            /* webpackChunkName: "dashboard" */ "@/views/dashboard/index.vue"
+          ),
+        meta: {
+          title: "Dashboard",
+          icon: "dashboard"
+        }
+      }
+    ]
+  }
+]
+export const routes = [...constantRoutes, ...asyncRoutes]
+
 export default createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: Layout,
-      redirect: "/dashboard",
-      children: [
-        {
-          path: "dashboard",
-          name: "Dashboard",
-          component: () =>
-            import(
-              /* webpackChunkName: "dashboard" */
-              "@/views/dashboard/index.vue"
-            ),
-          meta: {
-            title: "Dashboard" // 需要配置声明文件否则无提示
-          }
-        }
-      ]
-    },
-    {
-      path: "/home",
-      component: () => import("../views/Home.vue")
-    },
-    {
-      path: "/about",
-      component: () => import("../views/About.vue")
-    }
-  ]
+  routes
 })

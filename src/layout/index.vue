74befb5a81b756943211346ded6b3057c105e6db
wangtongmeng <tongmengwang@163.com>
2022-11-28 08:52:59 +0800

    feat: 实现 tagsview 基本功能

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -6,7 +6,7 @@
     <div class="main-container">
       <div class="header">
         <navbar></navbar>
-        <div class="tags-view">tagsview</div>
+        <tags-view></tags-view>
       </div>
       <div class="app-main">
         <app-main></app-main>
